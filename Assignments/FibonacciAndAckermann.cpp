a062ea78c647ed2569fe780da4406d307f7e6dea
MrBabuls <111220733+MrBabuls@users.noreply.github.com>
2025-09-19 09:28:23 +0300

    Fix Fibonacci memory measurement
    Changed Fibonacci to compare memory usage instead of execution time. Corrected base cases to standard Fibonacci definition.

--- Assignments/FibonacciAndAckermann.cpp
+++ Assignments/FibonacciAndAckermann.cpp
@@ -1,10 +1,8 @@
 #include "FibonacciAndAckermann.h"
 #include <iostream>
 #include <cstdlib>
-#include <chrono>
 
 using namespace std;
-using namespace std::chrono;
 
 // Iterative Fibonacci using array
 long long fib_iterative(int n) 
@@ -15,7 +13,7 @@ long long fib_iterative(int n)
     }
 
     long long* array = new long long[n + 1];
-    array[0] = 1;
+    array[0] = 0;
     array[1] = 1;
 
     for (int i = 2; i <= n; i++) 
@@ -36,7 +34,7 @@ long long fib_optimized(int n)
         return n;
     }
 
-    long long a = 1, b = 1;
+    long long a = 0, b = 1;
     for (int i = 2; i <= n; i++) 
     {
         long long temp = a + b;
@@ -53,20 +51,12 @@ void RunFibonacci()
     int n = 20;
 
     cout << "--- Fibonacci using array ---\n";
-    auto start1 = high_resolution_clock::now();
     long long fib1 = fib_iterative(n);
-    auto end1 = high_resolution_clock::now();
-    auto duration1 = duration_cast<microseconds>(end1 - start1).count();
-
     cout << "Fibonacci number f(" << n << ") = " << fib1 << "\n";
-    cout << "Time taken: " << duration1 << " ms\n\n";
+    cout << "Memory used: " << (n + 1) * sizeof(long long) << " bytes\n\n";
 
     cout << "--- Optimized Fibonacci ---\n";
-    auto start2 = high_resolution_clock::now();
     long long fib2 = fib_optimized(n);
-    auto end2 = high_resolution_clock::now();
-    auto duration2 = duration_cast<microseconds>(end2 - start2).count();
-
     cout << "Fibonacci number f(" << n << ") = " << fib2 << "\n";
-    cout << "Time taken: " << duration2 << " ms\n";
+    cout << "Memory used: " << 2 * sizeof(long long) << " bytes\n";
 }
